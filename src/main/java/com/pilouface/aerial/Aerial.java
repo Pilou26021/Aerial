a531b8c9bf93d0900bca2d7e7a799c47d413001a
PilouFace <24657574+Pilou26021@users.noreply.github.com>
2026-01-15 22:09:23 +0100

    Base check for wings in the chest slot bleh

--- src/main/java/com/pilouface/aerial/Aerial.java
+++ src/main/java/com/pilouface/aerial/Aerial.java
@@ -1,10 +1,14 @@
 package com.pilouface.aerial;
 
 import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.event.events.player.PlayerDisconnectEvent;
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.pilouface.aerial.system.AerialFlightSystem;
 
 public class Aerial extends JavaPlugin {
+    private AerialFlightSystem movementSystem;
 
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
@@ -12,4 +16,15 @@ public class Aerial extends JavaPlugin {
         super(init);
         LOGGER.atInfo().log("Hello from %s version %s", this.getName(), this.getManifest().getVersion().toString());
     }
+
+    protected void setup() {
+
+        this.movementSystem = new AerialFlightSystem();
+        this.getEntityStoreRegistry().registerSystem(this.movementSystem);
+        this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, (event) -> this.movementSystem.handlePlayerReady(event.getPlayer().getUuid()));
+        this.getEventRegistry().registerGlobal(PlayerDisconnectEvent.class, (event) -> this.movementSystem.handlePlayerDisconnect(event.getPlayerRef().getUuid()));
+    }
+
+
+
 }
---
+++ src/main/java/com/pilouface/aerial/system/AerialFlightSystem.java
@@ -0,0 +1,113 @@
+// java
+package com.pilouface.aerial.system;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.builtin.mounts.MountedComponent;
+import com.hypixel.hytale.component.ArchetypeChunk;
+import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.ComponentType;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.dependency.Dependency;
+import com.hypixel.hytale.component.dependency.Order;
+import com.hypixel.hytale.component.dependency.SystemDependency;
+import com.hypixel.hytale.component.query.Query;
+import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
+import com.hypixel.hytale.math.vector.Vector3d;
+import com.hypixel.hytale.math.vector.Vector3f;
+import com.hypixel.hytale.protocol.Color;
+import com.hypixel.hytale.protocol.GameMode;
+import com.hypixel.hytale.protocol.MovementStates;
+import com.hypixel.hytale.server.core.HytaleServer;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockMovementSettings;
+import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.entity.movement.MovementStatesComponent;
+import com.hypixel.hytale.server.core.inventory.Inventory;
+import com.hypixel.hytale.server.core.inventory.ItemStack;
+import com.hypixel.hytale.server.core.inventory.container.ItemContainer;
+import com.hypixel.hytale.server.core.modules.entity.component.CollisionResultComponent;
+import com.hypixel.hytale.server.core.modules.entity.component.PositionDataComponent;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.modules.entity.damage.DeathComponent;
+import com.hypixel.hytale.server.core.modules.entity.player.PlayerProcessMovementSystem;
+import com.hypixel.hytale.server.core.modules.entity.teleport.PendingTeleport;
+import com.hypixel.hytale.server.core.modules.entity.teleport.Teleport;
+import com.hypixel.hytale.server.core.modules.physics.component.Velocity;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import javax.annotation.Nonnull;
+
+
+public class AerialFlightSystem extends EntityTickingSystem<EntityStore> {
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    private final ComponentType<EntityStore, Player> playerComponentType;
+    private final ComponentType<EntityStore, PlayerRef> playerRefComponentType;
+    private final ComponentType<EntityStore, TransformComponent> transformComponentType;
+    private final ComponentType<EntityStore, Velocity> velocityComponentType;
+    private final ComponentType<EntityStore, MovementStatesComponent> movementStatesComponentType;
+    private final ComponentType<EntityStore, CollisionResultComponent> collisionResultComponentType;
+    private final ComponentType<EntityStore, PositionDataComponent> positionDataComponentType;
+    private final Query<EntityStore> query;
+    private final Map<UUID, PlayerState> states = new HashMap<>();
+
+    public AerialFlightSystem() {
+        this.playerComponentType = Player.getComponentType();
+        this.playerRefComponentType = PlayerRef.getComponentType();
+        this.transformComponentType = TransformComponent.getComponentType();
+        this.velocityComponentType = Velocity.getComponentType();
+        this.movementStatesComponentType = MovementStatesComponent.getComponentType();
+        this.collisionResultComponentType = CollisionResultComponent.getComponentType();
+        this.positionDataComponentType = PositionDataComponent.getComponentType();
+        this.query = Query.and(this.playerComponentType, this.playerRefComponentType, this.transformComponentType, this.velocityComponentType, this.movementStatesComponentType, this.collisionResultComponentType, this.positionDataComponentType);
+    }
+
+    @Nonnull
+    public Query<EntityStore> getQuery() {
+        return this.query;
+    }
+
+    public boolean isParallel(int archetypeChunkSize, int taskCount) {
+        return false;
+    }
+
+    public void handlePlayerReady(@Nonnull UUID playerUuid) {
+        PlayerState state = (PlayerState)this.states.computeIfAbsent(playerUuid, (k) -> new PlayerState());
+    }
+
+    public void handlePlayerDisconnect(@Nonnull UUID playerUuid) {
+        this.states.remove(playerUuid);
+    }
+
+    @Override
+    public void tick(float deltaSeconds, int startIndex, ArchetypeChunk<EntityStore> chunk, Store<EntityStore> store, CommandBuffer<EntityStore> commandBuffer) {
+        Player player = (Player)chunk.getComponent(startIndex, this.playerComponentType);
+        if (player == null) return;
+        if(this.hasWings(player).equals(1)) {
+        }
+    }
+
+    public Integer hasWings(@Nonnull Player player) {
+        Inventory inventory = player.getInventory();
+        ItemContainer armor = inventory.getArmor();
+        ItemStack chest = armor.getItemStack((short) 1);
+
+        if (chest.getItemId().equals("Aerial_Wings_T1") || chest.getItemId().equals("Aerial_Wings_T2") || chest.getItemId().equals("Aerial_Wings_T3")) {
+            return 1;
+        }
+
+        return 0;
+
+    }
+
+    private static final class PlayerState {
+        private PlayerState() {}
+    }
+}
